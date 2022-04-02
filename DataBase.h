661fe6e7a404f6205fbc07fbe1a5d458c232dd0b
DanPolev <dpp1997@rambler.ru>
2022-04-02 21:09:57 +0700

    Add typedef for maps in DataBase.h

--- DataBase.h
+++ DataBase.h
@@ -2,15 +2,18 @@
 
 #include <map>
 #include <functional>
+#include <utility>
 //#include <string>
 
 #include "User.h"
 
+typedef std::map<std::string, std::string> Tstringmap;
+typedef std::map<std::string, std::function<void()>> Tstring_functionMap;
+
 class DataBase {
 private:
-    // TODO: add m_id here
-    std::map<std::string, std::string> m_userPasswdMap;
-    std::map<std::string, std::function<void()>> m_funcmap;
+    Tstringmap m_userPasswdMap;
+    const Tstring_functionMap m_funcmap { {"chn", &changeUsername}};
     const std::string m_filename = "DataBase.txt";
     User m_currUser;
 public:
